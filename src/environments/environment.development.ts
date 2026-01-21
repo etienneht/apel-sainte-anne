eb9aaade94b8e2683b10f3e3bd918d4fcf289640
AntoineLudwigGalli <antoineludwiggalli@gmail.com>
2026-01-21 11:04:49 +0100

    intégration firebase avec initialisation hosting, firestore et storage. Modif import Bootstrap pour être ESM compliant

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAOzO4LGb3DmgDe8rIWnLungf3X7HJ27sk",
+
+    authDomain: "apel-sainte-anne.firebaseapp.com",
+
+    projectId: "apel-sainte-anne",
+
+    storageBucket: "apel-sainte-anne.firebasestorage.app",
+
+    messagingSenderId: "697275691738",
+
+    appId: "1:697275691738:web:44d7760b613f69f9a46be3",
+
+    measurementId: "G-8YFCXV2651"
+
+  }
+};
