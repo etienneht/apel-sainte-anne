ff535683a5d8bc39073dd9a36c7a26c2649c3ea3
AntoineLudwigGalli <antoineludwiggalli@gmail.com>
2026-01-27 16:27:11 +0100

    reintegration environments dans versioning pour CI

---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBId60eRdkauw2yClrJyquLR5RRaPhCbys",
+
+    authDomain: "apel-sainte-anne.firebaseapp.com",
+
+    projectId: "apel-sainte-anne",
+
+    storageBucket: "apel-sainte-anne.firebasestorage.app",
+
+    messagingSenderId: "697275691738",
+
+    appId: "1:697275691738:web:44d7760b613f69f9a46be3",
+
+    measurementId: "G-8YFCXV2651"
+
+  },
+
+  baseUrl: 'https://apel-sainte-anne.web.app',
+};
